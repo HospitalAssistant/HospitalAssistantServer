f15656f4a13f89439c27923727bcb6622c0fc7f7
HospitalAssistant <hospital_assistant@abv.bg>
2019-11-17 19:40:32 +0200

    enable dialogflow to call us without providing credentials

--- hospital-assistant-controller/src/main/java/com/hospital/assistant/config/SecurityConfig.java
+++ hospital-assistant-controller/src/main/java/com/hospital/assistant/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/storage/**", "/api/auth/**")
+                .antMatchers("/storage/**", "/api/auth/**", "/hospital/assistant/test")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
